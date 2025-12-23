8ca34f5c25a6a7e7a678a86bbf0bc9103ac5e513
Asmita Saha <sahaasmita60@gmail.com>
2025-12-24 01:24:45 +0530

    Update score.py

--- score.py
+++ score.py
@@ -6,17 +6,30 @@ class Score(Turtle):
         self.penup()
         self.hideturtle()
         self.level=1
+        with open("score.txt") as board:
+            self.highlevel=int(board.read())
         self.createlevel()
     def createlevel(self):
         self.clear()
-        self.goto(-200,250)
-        self.write(f"Level={self.level}", align="center",font=("Arial",20,"normal"))
+        self.goto(-130,250)
+        self.write(f"Level={self.level}, Highest Level={self.highlevel}", align="center",font=("Arial",20,"normal"))
 
     def inclevel(self):
         self.level+=1
         self.createlevel()
 
-    def gameover(self):
-        self.clear()
-        self.goto(0,0)
-        self.write(f"Game Over", align="center",font=("Arial",50,"normal"))
+
+    def reset(self):
+        if self.level>self.highlevel:
+            self.highlevel=self.level
+            with open("score.txt") as scoretext:
+                file=scoretext.read()
+                content=int(file)
+                if content<self.highlevel:
+                    with open("score.txt","w") as write:
+                        self.highestscore=str(self.highlevel)
+                        write.write(self.highestscore)
+
+        self.score=0
+        self.createlevel()
+        
